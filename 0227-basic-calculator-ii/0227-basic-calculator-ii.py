f2282adae8e3b3df48a6c15332ff6fe62daa6f28
annalakshmia910-art <annalakshmia910@gmail.com>
2026-07-22 09:13:30 +0530

    Time: 35 ms (97.38%), Space: 22.6 MB (25.09%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = "+"
+
+        s += "+"
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            elif ch in "+-*/":
+
+                if sign == "+":
+                    stack.append(num)
+
+                elif sign == "-":
+                    stack.append(-num)
+
+                elif sign == "*":
+                    stack.append(stack.pop() * num)
+
+                elif sign == "/":
+                    stack.append(int(stack.pop() / num))
+
+                sign = ch
+                num = 0
+
+        return sum(stack)
