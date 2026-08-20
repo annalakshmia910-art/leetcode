a38324a83615c8abaac82b60e32bb0d6d4e832c9
annalakshmia910-art <annalakshmia910@gmail.com>
2026-08-20 21:30:24 +0530

    Time: 248 ms (5.01%), Space: 19.2 MB (92.17%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+
+        for h in range(n, 0, -1):
+            count = 0
+
+            for citation in citations:
+                if citation >= h:
+                    count += 1
+
+            if count >= h:
+                return h
+
+        return 0
